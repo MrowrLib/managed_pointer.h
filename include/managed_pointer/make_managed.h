fbbfc5bc088ee1fd40e8a29ac72c2e35e95841af
Mrowr Purr <mrowr.purr@gmail.com>
2023-12-31 14:38:39 -0800

    Remove the make_managed with a T*
    It caused issues deducing the Args&& one

--- include/managed_pointer/make_managed.h
+++ include/managed_pointer/make_managed.h
@@ -8,9 +8,4 @@ namespace ManagedPointer {
     inline managed_ptr<T> make_managed(Args&&... args) {
         return managed_ptr<T>(new T(std::forward<Args>(args)...));
     }
-
-    template <typename T>
-    inline managed_ptr<T> make_managed(T* ptr, bool deletes_pointer = true) {
-        return managed_ptr<T>(ptr, deletes_pointer);
-    }
 }
